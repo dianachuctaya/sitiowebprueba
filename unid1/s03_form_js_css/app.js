2a6d760594392c36425bc8eec6a7581a5db1eb78
dianachuctaya <dianachuctayapachecor@gmail.com>
2020-05-26 23:26:28 -0500

    Sem. 3 table - funtion

---
+++ unid1/s03_form_js_css/app.js
@@ -0,0 +1,9 @@
+
+function fnMensaje(){
+    var xValor = "funtion 1";
+    alert("Hola mundo... " + xValor);
+}
+function fnMensaje2() {
+    var xValor = "function 2";
+    document.writeln("Hola mundo... " + xValor);
+}
